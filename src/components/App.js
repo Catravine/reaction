97b43fcdd9ee9bcdd0e795bd8297b6667ade0c6f
Caroline Courtney <caroline@carolinecourtney.com>
2019-09-08 11:49:24 -0400

    refactor useContext into custom app hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../state/reducer';
-import PublishMessage from './PublishMessage'
+import Context from '../context';
+import PublishMessage from './PublishMessage';
+import MessageBoard from './MessageBoard';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -8,11 +10,13 @@ function App() {
   console.log('state', state)
 
   return (
-    <div>
+    <Context.Provider value={{ state, dispatch }}>
       <h2>Reaction</h2>
       <hr />
-      <PublishMessage dispatch={dispatch}/>
-    </div>
+      <PublishMessage/>
+      <hr />
+      <MessageBoard/>
+    </Context.Provider>
   );
 }
 
